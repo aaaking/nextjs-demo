21b3e332743bc3319c3baf9e53c69dfae0f32cfb
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2025-08-06 02:32:41 +0800

    fs

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
 
 
 
@@ -9,7 +10,8 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      {/* <body>{children}</body> */}
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
