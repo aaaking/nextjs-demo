8ab108356a4754c4d4aade1843573c372a73037a
zhouzhihui <wb_zhouzhihui@kuaishou.com>
2025-08-06 19:40:06 +0800

    fs

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,7 +1,7 @@
 import SideNav from '@/app/ui/dashboard/sidenav';
 
+// https://nextjs.org/learn/dashboard-app/partial-prerendering
 export const experimental_ppr = true;
-
  
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
